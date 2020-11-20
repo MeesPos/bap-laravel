37e19d7506e9aaf645347d8a556618e586d18c63
MeesPos <meespostma_@hotmail.com>
2020-11-20 11:18:50 +0100

    Bezig met home pagina

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@home')->name('home');
+Route::get('/producten', 'ProductController@producten')->name('producten');
+Route::get('/product/{id}/{name}', 'ProductController@productDetail')->name('product');
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,11 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+
+const tailwindcss = require('tailwindcss')
+
+mix.sass('resources/sass/main.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+        postCss: [ tailwindcss('tailwind.config.js') ]
+    })
