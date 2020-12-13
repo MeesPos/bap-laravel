c9df5c41c9fee57f7f2e7efd94fff1d0538e23a1
MeesPos <meespostma_@hotmail.com>
2020-12-13 13:04:10 +0100

    Assets opgelost

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Designia</title>
-    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 
@@ -25,7 +25,7 @@
         @show
     </main>
 
-    <script src="/js/app.js"></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
     <script>
         document.getElementById("toggle").addEventListener("click", function() {
             if(document.getElementById("nav-menu").style.display === "none") {
